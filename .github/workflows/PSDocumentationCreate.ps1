e3697709791e09d08e8254875413184bdf311fb8
Brandon Casey <bcasey266@gmail.com>
2023-03-14 09:24:34 -0500

    Update Documentation Automation

--- .github/workflows/PSDocumentationCreate.ps1
+++ .github/workflows/PSDocumentationCreate.ps1
@@ -1,3 +1,5 @@
+$InformationPreference = 'Continue'
+
 Install-Module platyps -Force
 Import-Module platyps
 
@@ -7,7 +9,7 @@ foreach ($PowershellFile in $PowershellFiles) {
     Set-Location $PowershellFile.Directory.FullName
 
     New-MarkdownHelp -Command ".\$($PowershellFile.Name)" -OutputFolder .\ -NoMetadata -Force -ErrorAction SilentlyContinue | Out-Null
-    
+
     if (Test-Path "$($PowershellFile.Name).md") {
         Write-Information "Successfully created documentation for $($PowershellFile.Name)"
         Move-Item -Destination README.md -Path "$($PowershellFile.Name).md" -Force
