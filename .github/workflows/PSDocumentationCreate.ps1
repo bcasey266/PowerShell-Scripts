c77a5d3cbcc005fd7f307c746b5314b4073eddb5
Brandon Casey <bcasey266@gmail.com>
2023-03-14 09:16:18 -0500

    Updated Documentation Automation

--- .github/workflows/PSDocumentationCreate.ps1
+++ .github/workflows/PSDocumentationCreate.ps1
@@ -6,5 +6,13 @@ $PowershellFiles = Get-ChildItem *.ps1 -Recurse
 foreach ($PowershellFile in $PowershellFiles) {
     Set-Location $PowershellFile.Directory.FullName
 
-    New-MarkdownHelp -Command ".\$($PowershellFile.Name)" -OutputFolder .\ -NoMetadata -Force -ErrorAction SilentlyContinue | Move-Item -Destination README.md -Force
+    New-MarkdownHelp -Command ".\$($PowershellFile.Name)" -OutputFolder .\ -NoMetadata -Force -ErrorAction SilentlyContinue | Out-Null
+    
+    if (Test-Path "$($PowershellFile.Name).md") {
+        Write-Information "Successfully created documentation for $($PowershellFile.Name)"
+        Move-Item -Destination README.md -Path "$($PowershellFile.Name).md" -Force
+    }
+    else {
+        Write-Warning "Unable to create documentation for $($PowershellFile.Name)"
+    }
 }
